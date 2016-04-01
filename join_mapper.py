ac69a4bb06df5df2eb2da51b30fdcb1933728b68
Chris19920210 <chen3809@umn.edu>
2016-04-01 10:23:58 -0500

    test3

--- join_mapper.py
+++ join_mapper.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
 import sys
 
-# --------------------------------------------------------------------------
-#This mapper code will input a <date word, value> input file, and move date into
-#  the value field for output
-#
-#  Note, this program is written in a simple style and does not full advantage of Python
-#     data structures,but I believe it is more readable
-#
-#  Note, there is NO error checking of the input, it is assumed to be correct
-#     meaning no extra spaces, missing inputs or counts,etc..
-#
-# See #  see https://docs.python.org/2/tutorial/index.html for details  and python  tutorials
-#
-# --------------------------------------------------------------------------
+
 for line in sys.stdin:
 
     line = line.strip()   #strip out carriage return
